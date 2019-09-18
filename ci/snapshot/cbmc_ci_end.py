e0881d08d704d4c91f4a18350f4a3e841d29cbbd
Jonathan Eidelman <jeid@amazon.com>
2019-09-18 16:02:14 -0500

    Should only try to copy on report

--- ci/snapshot/cbmc_ci_end.py
+++ ci/snapshot/cbmc_ci_end.py
@@ -83,20 +83,25 @@ class S3Manager:
 class Job_name_info:
 
     def __init__(self, job_name):
-        job_name_match = self.check_job_name(job_name)
+        job_name_match = self.check_job_name(job_name, "property")
+        report_name_match = self.check_job_name(job_name, "report")
         if job_name_match:
             self.is_cbmc_batch_property_job = True
+            self.job_name = job_name_match.group(1)
+            self.timestamp = job_name_match.group(2)
+        elif report_name_match:
+            self.is_report_job = True
             self.job_name = job_name_match.group(1)
             self.timestamp = job_name_match.group(2)
         else:
             self.is_cbmc_batch_property_job = False
 
     @staticmethod
-    def check_job_name(job_name):
+    def check_job_name(job_name, suffix):
         """Check job_name to see if it matches CBMC Batch naming conventions"""
         job_name_pattern = r"([\S]+)"
         timestamp_pattern = r"(\S{16})"
-        pattern = job_name_pattern + timestamp_pattern + "-property$"
+        pattern = job_name_pattern + timestamp_pattern + "-"+suffix+"$"
         res = re.search(pattern, job_name)
         return res
 
@@ -159,16 +164,48 @@ def lambda_handler(event, context):
             if expected in cbmc:
                 print("Expected Verification Result: {}".format(s3_dir))
                 update_status(
+                    "pending", job_dir, s3_dir, desc, repo_id, sha, is_draft)
+            else:
+                print("Unexpected Verification Result: {}".format(s3_dir))
+                update_status(
+                    "pending", job_dir, s3_dir, desc, repo_id, sha, is_draft)
+
+        except Exception as e:
+            traceback.print_exc()
+            # CBMC Error
+            desc += ": CBMC Error"
+            print(desc)
+            update_status("error", job_dir, s3_dir, desc, repo_id, sha, False)
+            raise e
+    elif status in ["SUCCEEDED", "FAILED"] and job_name_info.is_report_job:
+        # Copy HTML results to HTML bucket
+        # Waits for HTML files to appear - may time out
+        s3_manager.copy_to_html_bucket(job_name_info.get_s3_html_dir())
+
+        s3_dir = job_name_info.get_s3_dir()
+        job_dir = job_name_info.get_job_dir()
+        # Prepare description for GitHub status update
+        desc = "CBMC Batch job " + s3_dir + " " + status
+        # Get bookkeeping information about commit
+        repo_id = int(s3_manager.read_from_s3(s3_dir + "/repo_id.txt"))
+        sha = s3_manager.read_from_s3(s3_dir + "/sha.txt").decode('ascii')
+        draft_status = s3_manager.read_from_s3(s3_dir + "/is_draft.txt").decode('ascii')
+        is_draft = draft_status.lower() == "true"
+        # Get CBMC output
+        cbmc = s3_manager.read_from_s3(s3_dir + "/out/cbmc.txt")
+        try:
+            # Get expected output substring
+            expected = s3_manager.read_from_s3(s3_dir + "/expected.txt")
+            # Get CBMC output
+            cbmc = s3_manager.read_from_s3(s3_dir + "/out/cbmc.txt")
+            if expected in cbmc:
+                print("Expected Verification Result: {}".format(s3_dir))
+                update_status(
                     "success", job_dir, s3_dir, desc, repo_id, sha, is_draft)
             else:
                 print("Unexpected Verification Result: {}".format(s3_dir))
                 update_status(
                     "failure", job_dir, s3_dir, desc, repo_id, sha, is_draft)
-
-            # Copy HTML results to HTML bucket
-            # Waits for HTML files to appear - may time out
-            s3_manager.copy_to_html_bucket(job_name_info.get_s3_html_dir())
-
 
         except Exception as e:
             traceback.print_exc()
