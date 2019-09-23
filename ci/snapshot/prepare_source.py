62739a780c1caa3a13714e1ecd956fda39727812
Jonathan Eidelman <jeid@amazon.com>
2019-09-23 14:10:34 -0400

    Only run 3 proofs

--- ci/snapshot/prepare_source.py
+++ ci/snapshot/prepare_source.py
@@ -327,7 +327,7 @@ def generate_cbmc_jobs(src, repo_id, repo_sha, is_draft, tarfile):
     tasks = find_tasks(PROOF_MARKERS, src)
     print("{} tasks found".format(len(tasks)))
 
-    for (proofname, proofdir) in tasks:
+    for (proofname, proofdir) in tasks[:3]:
         # pylint: disable=broad-except
         try:
             # Try to run batch
