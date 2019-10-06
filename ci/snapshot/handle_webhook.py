120300d2c914b4ed4087df985d32878803b466b5
Jonathan Eidelman <jeid@amazon.com>
2019-10-06 11:06:53 -0400

    Externalize invoke batch lambda

--- ci/snapshot/handle_webhook.py
+++ ci/snapshot/handle_webhook.py
@@ -21,7 +21,7 @@ SUCCEEDED = "COMPLETED:SUCCEEDED"
 FAILED = "COMPLETED:FAILED"
 LAUNCH_SUCCEEDED = "LAUNCHED:SUCCEEDED"
 LAUNCH_FAILED = "LAUNCHED:FAILED"
-
+INVOKE_BATCH_LAMBDA_FN_NAME = os.getenv("INVOKE_BATCH_LAMBDA_FN_NAME")
 def is_completed_status(status):
   return status == SUCCEEDED or status == FAILED or status == IGNORED
 
@@ -147,7 +147,7 @@ def lambda_handler(event, context):
           event['correlation_list'] = logger.create_child_correlation_list()
           logger.launch_child("cbmc_ci_start:lambda_handler", None, event['correlation_list'])
           result = lc.invoke(
-              FunctionName='InvokeBatchLambda',
+              FunctionName=INVOKE_BATCH_LAMBDA_FN_NAME,
               Payload=json.dumps(event))
           response['statusCode'] = result['StatusCode']
   except Exception as e:
