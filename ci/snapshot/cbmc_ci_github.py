56590995d0dc4864966fdc0c9c2b9bc0aed02edd
Jonathan Eidelman <jeid@amazon.com>
2020-06-04 11:28:19 -0400

    Fixed error where cloudfront url is none

--- ci/snapshot/cbmc_ci_github.py
+++ ci/snapshot/cbmc_ci_github.py
@@ -13,7 +13,7 @@ from cbmc_ci_timer import Timer
 CBMC_RETRY_KEYWORDS = ["CBMC_RETRY", "/cbmc run checks"]
 
 def update_github_status(repo_id, sha, status, ctx, desc, jobname, post_url = False):
-    target_url = ""
+    target_url = None
     if jobname and post_url:
         cloudfront_url = os.environ['CLOUDFRONT_URL']
         target_url = (f"https://{cloudfront_url}/{jobname}/out/html/index.html")
