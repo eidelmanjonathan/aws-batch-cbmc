1d17123fc58d1b393ebf08dc435cde8861d4e199
Jonathan Eidelman <jeid@amazon.com>
2020-06-04 10:08:28 -0400

    Requeue on failure

--- ci/snapshot/github_worker_lambda.py
+++ ci/snapshot/github_worker_lambda.py
@@ -1,12 +1,19 @@
-import boto3
-import logging
-import github
+import os
 import json
-from math import ceil
+from math import floor, ceil
 from datetime import datetime
 from time import sleep
 from update_github import GithubUpdater
 
+import boto3
+THROTTLE_THRESHOLD = 300
+TIMEOUT_LIMIT = 500
+
+queue_url = os.getenv("QUEUE_URL")
+
+def enqueue_message(msg):
+    sqs = boto3.client("sqs")
+    sqs.send_message(QueueUrl=queue_url, MessageBody=msg)
 
 def lambda_handler(event, request):
     print(event)
@@ -14,18 +21,27 @@ def lambda_handler(event, request):
     for r in event["Records"]:
         github_msg = json.loads(r["body"])
         print(json.dumps(github_msg, indent=2))
+
+        # We should only create the GithubUpdater once
+        # since it uses up some of our API limit
         if g is None:
             g = GithubUpdater(repo_id=int(github_msg["repo_id"]),
                               oath_token=github_msg["oath"])
 
         remaining_calls = g.get_rate_limit()
         time_to_reset = g.get_reset_time() - datetime.now()
-        seconds = ceil(time_to_reset.total_seconds())
+        seconds = floor(time_to_reset.total_seconds())
         print(f"Rate limit remaining: {remaining_calls}")
         print(f"Seconds to reset: {seconds}")
-        if remaining_calls <= seconds:
-            multiplier = ceil(seconds / remaining_calls)
-            print(f"Sleeping for {multiplier} seconds")
-            sleep(multiplier)
+        if remaining_calls < THROTTLE_THRESHOLD and remaining_calls <= seconds:
+            sleep_time = ceil(seconds / remaining_calls)
+
+            if sleep_time >= TIMEOUT_LIMIT:
+                sleep(TIMEOUT_LIMIT)
+                enqueue_message(json.dumps(github_msg))
+                return
+
+            print(f"Running low on github Sleeping for {sleep_time} seconds")
+            sleep(sleep_time)
 
         g.update_status(status=github_msg["status"], proof_name=github_msg["context"], commit_sha=github_msg["commit"])
--- ci/snapshot/update_github.py
+++ ci/snapshot/update_github.py
@@ -4,36 +4,22 @@ from datetime import datetime
 
 import github
 
-OATH_TOKEN = os.getenv("GIT_OATH")
-CLOUDFRONT_URL = os.getenv('CLOUDFRONT_URL')
-GIT_REPO = os.getenv("GIT_REPO")
-GIT_OATH = os.getenv("GIT_OATH")
-REPO_OWNER = os.getenv("GIT_OWNER")
-GIT_COMMIT_SHA = os.getenv("GIT_COMMIT_SHA")
-UUID = os.getenv("UUID")
 class GithubUpdater:
     GIT_SUCCESS = "success"
     GIT_FAILURE = "failure"
-    LITANI_SUCCESS = "successful"
-    LITANI_FAILURE = "failed"
 
-    def __init__(self, repo_id=None, oath_token=None, cloudfront_url=None, session_uuid=None, status_json_filename = None):
+    def __init__(self, repo_id=None, oath_token=None, session_uuid=None):
         self.session_uuid = session_uuid
         self.g = github.Github(oath_token)
         self.repo = self.g.get_repo(repo_id)
-        self.cloudfront_url = cloudfront_url
-        if status_json_filename:
-            with open(status_json_filename) as f:
-                self.proof_statuses = json.loads(f.read())
-        print(f"Created GithubUpdater for cloudfront: {cloudfront_url} with uuid: {session_uuid}")
 
-    def update_status(self, status=GIT_SUCCESS, proof_name=None, commit_sha=None,):
+    def update_status(self, status=GIT_SUCCESS, proof_name=None, commit_sha=None, cloudfront_url=None):
         kwds = {'state': status,
                 'context': proof_name,
                 'description': "Description",
-                'target_url': (f"https://{self.cloudfront_url}/{self.session_uuid}/{proof_name}/report/report/index.html")}
+                'target_url': cloudfront_url}
+        print(f"Updating github status with the following parameters:\n{json.dumps(kwds, indent=2)}")
         self.repo.get_commit(sha=commit_sha).create_status(**kwds)
-        print(json.dumps(kwds))
         return
 
     def get_rate_limit(self):
